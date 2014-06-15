79a1d8a2385c87ece8ae209bacfb3d5da45fe0d8
zhelonkin <24.04.sasha@gmail.com>
2014-06-15 13:17:47 +0400

    лабораторная 3

--- lab31.php
+++ lab31.php
@@ -6,30 +6,24 @@
         <link rel="stylesheet" href="css/style.css" />
 	</head>
 	<body>
-	
 	<form action="lab31.php" method="get"
 		enctype="application/x-www-form-urlencoded">
 		<div class="sign_up">Sign Up</div>
-		<div>It's free and always will be.</div>
-		<div>First Name:
-            <input class="text_area" type="text" name="first_name" /><div>
-		<div>Last Name: 
-            <input class="text_area" type="text" name="last_name" /><div>
-		<div>Your Email:
-            <input class="text_area" type="text" name="email" /><div>
-		<div>Re-enter Email:
-            <input class="text_area" type="text" name="re_email" /><div>
-		<div>New Password:
-            <input class="text_area" type="text" name="password" /><div>
-		<div>I am:
-            <select name="sex">
+		<div class="It">It's free and always will be.</div>
+		<p>First Name: <input class="text_area" type="text" name="first_name"></p>
+		<p>Last Name:  <input class="text_area" type="text" name="last_name"></p>
+        <p>Your Email: <input class="text_area" type="text" name="email"></p>
+		<p>Re-enter Email: <input class="text_area" type="text" name="re_email"></p>
+		<p>New Password:   <input class="text_area" type="text" name="password"></p>
+		<p>I am:
+            <select class="sex_text" name="sex">
                 <option selected hidden>Select sex:</option>
                 <option>male</option>
                 <option>female</option>
-            </select><div>
-		<div>Birthday:
-      
-        <select class="list" name="Month">
+            </select>
+        </p>
+		<p>Birthday:
+        <select class="month_text" name="Month">
             <option selected hidden>Month:</option>
             <option value="1">Jan</option>
             <option value="2">Feb</option>
@@ -45,7 +39,7 @@
             <option value="12">Dec</option>
         </select>
         
-        <select class="list" name="Day">
+        <select class="day_text" name="Day">
             <option selected hidden>Day:</option>
             <?php
               for ( $day = 1; $day < 31; $day++ )
@@ -54,19 +48,17 @@
             <?php  }  ?>
         </select>     
         
-        <select class="list" name="Year">
+        <select class="year_text" name="Year">
             <option selected hidden>Year:</option>
             <?php
               for ( $year = 1900; $year < 2014; $year++ )
               { ?>
                 <option><?php echo $year; ?></option>
             <?php  }  ?> 
-        </select></div>
-      
-		<div>Why do I need to provide this?<div>
-        <button class="button"></button>
+        </select>
+        </p>
+		<div class="Why_do">Why do I need to provide this?</div>
+        <input class="button" type="image" src="images/sign_up.png">
 	</form>
-	
-	
 	</body>
 </html>
